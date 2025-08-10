84328dcc49e23c92ecd8e2c8f9219c842268d840
cheynik2004 <140960891+cheynik2004@users.noreply.github.com>
2025-08-10 12:43:24 +0530

    Add user authentication with DAO and DB connection
    Introduced DBConnection and UserDAO classes. Updated User model with fields and accessors. Modified LoginServlet. Added required dependencies for servlet

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,31 @@
 package model;
 
 public class User {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private int Id;
+    private String password;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
 }
